8a97e702024ea1e295bd5219e8847cff906c46f0
amid10dev <diwa@mail.ru>
2023-08-29 17:37:22 +0300

    Start leetcode

---
+++ src/Easy/MySqrt.java
@@ -0,0 +1,19 @@
+package Easy;
+
+import java.io.IOException;
+
+public class MySqrt {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(mySqrt(8));
+
+    }
+    public static int mySqrt(int x) {
+        int result = 0;
+        if (x > 0 && x < (Math.pow(2, 31)) - 1) {
+           result = (int) Math.sqrt(x);
+        } 
+        return result;
+    }
+
+}
---
+++ src/Easy/TwoSum.java
@@ -0,0 +1,23 @@
+package Easy;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arrayTest = new int[]{3, 2, 4};
+        int[] arrayResult = twoSum(arrayTest, 6);
+        for (int i = 0; i < arrayResult.length; i++) {
+            System.out.println(arrayResult[i]);
+        }
+    }
+    public static int[] twoSum(int[] nums, int targer) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++)
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == targer) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+
+        return result;
+    }
+}
