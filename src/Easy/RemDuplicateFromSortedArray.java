d34399eb313c2665dc841d1471c955f437682835
amid10dev <diwa@mail.ru>
2023-08-29 17:37:22 +0300

    +1 task (RemDuplicate)

---
+++ src/Easy/RemDuplicateFromSortedArray.java
@@ -0,0 +1,45 @@
+package Easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemDuplicateFromSortedArray {
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+        System.out.println(removeDuplicates(nums));
+        for (int e: nums
+             ) {
+            System.out.print(e);
+        }
+
+        System.out.println(removeDuplicatesUseSet(nums));
+
+        for (int e: nums
+        ) {
+            System.out.print(e);
+        }
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static int removeDuplicatesUseSet(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        return set.size();
+    }
+
+}
+
+
