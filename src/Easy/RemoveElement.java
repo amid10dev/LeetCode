8f216f58e73f7c286798c07d8278283589ca3d2e
amid10dev <diwa@mail.ru>
2023-08-30 20:27:26 +0300

    +1 task (RemoveElement)

---
+++ src/Easy/RemoveElement.java
@@ -0,0 +1,23 @@
+package Easy;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        int[] testNums = new int[]{3, 2, 2, 3};
+        System.out.println(removeElement(testNums, 3));
+
+        for (int a : testNums
+        ) {
+            System.out.print(a);
+        }
+    }
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
